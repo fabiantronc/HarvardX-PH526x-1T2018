ca82375f4e58ae7b2e80d1eacc93f9f4dc41f5c5
Fabian Troncoso <fabiantronc@users.noreply.github.com>
2018-04-15 23:44:35 -0300

    Create Tarea_1a.py

---
+++ Semana1/Tarea_1a.py
@@ -0,0 +1,5 @@
+# Solicitan crear una variable que contenga todas las letras del abecedario en minúsculas y mayúsculas, se debe usar la librería "string".
+
+import string
+
+alphabet = string.ascii_letters
