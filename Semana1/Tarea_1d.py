c248beca348ee3df27a101020bb7b8b9c8bf076b
Fabian Troncoso <fabiantronc@users.noreply.github.com>
2018-04-16 00:00:13 -0300

    Create Tarea_1d.py

---
+++ Semana1/Tarea_1d.py
@@ -0,0 +1,13 @@
+# Abraham Lincoln was a president during the American Civil War. His famous 1863 Gettysburg Address has been stored as address, and the counter function as defined in part 1c has been loaded. Use these to return a dictionary consisting of the count of each letter in this address, and save this as address_count.
+# Print address_count.
+
+	address = "Four score and seven years ago our fathers brought forth  \
+		Now we are engaged in a great civil war  testing whether that nation  or any nation so conceived and so dedicated upon this continent  \
+		Now we are engaged in a great civil war  testing whether that nation  or any nation so conceived   can long endure. We are met on a great battle... a new nation  \
+		Now we are engaged in a great civil war  testing whether that nation  or any nation so conceived as a final resting place for those who died here conceived in liberty  \
+		Now we are engaged in a great civil war  testing whether that nation  or any nation so conceived that the nation might live. This we may and dedicated to the proposition that all men are created equal. Now we are engaged in a great civil war  testing whether that nation  or any nation so conceived in all propriety do."
+	
+	address_count = counter(address)
+	
+  # Imprime el resultado.
+  print(address_count)
